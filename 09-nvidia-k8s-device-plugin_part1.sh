7781e5c2ec3b1ccf1fc1075956cc7978d58484be
Jim Lin <jimlintw922@gmail.com>
2018-07-04 11:47:37 +0800

    mod 09-nvidia-k8s-device-plugin

--- 09-nvidia-k8s-device-plugin_part1.sh
+++ 09-nvidia-k8s-device-plugin_part1.sh
@@ -1,5 +1,6 @@
 # INSTALLATION GUIDE
 # * https://kubernetes.io/docs/tasks/tools/install-kubeadm/
 # * https://kubernetes.io/docs/setup/independent/create-cluster-kubeadm/
+# * https://github.com/jinsenglin/k8s/blob/master/ha-master/configure-machine.sh
 
 # TURN OFF SWAP
